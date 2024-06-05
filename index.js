ac69223842bf21f3f1967c76465510610ccd7cf6
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-06-05 12:52:55 +0530

    Complete a Project
    Add a Profile like, edit feature

--- index.js
+++ index.js
@@ -30,7 +30,32 @@ app.get('/login', (req, res) => {
 app.get('/profile', isLoggedIn, async (req,res) => {
     let user = await userModel.findOne({email : req.user.email}).populate("posts");
     // user.populate('posts')
-    res.render("profile", {user})
+    res.render("profile", {user}) 
+})
+
+app.get('/like/:id', isLoggedIn, async (req,res) => {
+    let post = await postModel.findOne({_id : req.params.id}).populate("user");
+
+    if(post.likes.indexOf(req.user.userid) === -1)
+    {
+        post.likes.push(req.user.userid)
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid) , 1)
+    }
+
+    await post.save()
+    res.redirect("/profile")
+})
+
+
+app.get('/edit/:id', isLoggedIn, async (req,res) => {
+    let post = await postModel.findOne({_id : req.params.id}).populate("user");
+    res.render("edit", {post})
+})
+
+app.post('/update/:id', isLoggedIn, async (req,res) => {
+    let post = await postModel.findOneAndUpdate({_id : req.params.id}, {content : req.body.content}).populate("user");
+    res.redirect("/profile",)
 })
 
 
